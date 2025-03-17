a6991e84c01480eaf61c9342cf1064b8bc538c2a
diegomolina <diego.molina@crunchyk.com>
2025-03-16 21:12:56 -0600

    implements footer

--- src/app/shared-components/main-layout/main-layout.component.ts
+++ src/app/shared-components/main-layout/main-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { GlobalHeaderComponent } from "../global-header/global-header.component";
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterModule, GlobalHeaderComponent],
+  imports: [RouterModule, GlobalHeaderComponent, FooterComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
