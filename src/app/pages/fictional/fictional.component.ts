0c3fa2f8a2b1b183d37a294f221f36878b756310
diegomolina <diego.molina@crunchyk.com>
2025-04-27 04:11:07 -0600

    home page ver 2

--- src/app/pages/fictional/fictional.component.ts
+++ src/app/pages/fictional/fictional.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-fictional',
+  standalone: true,
   imports: [],
   templateUrl: './fictional.component.html',
   styleUrl: './fictional.component.css'
